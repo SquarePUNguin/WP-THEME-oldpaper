c14137258ef7e0488baf3fcdd3bf6b12778a8182
SquarePUNguin <D2_v0lk_2000@mail.ru>
2025-05-07 14:44:22 +0300

    release

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-if(! function_exists("theme_setup")){
-	function theme_setup(){
+if(! function_exists("WP_THEME_SquarePUNguin_theme_setup")){
+	function WP_THEME_SquarePUNguin_theme_setup(){
 		add_theme_support('custom-logo', [
 		'height'      => 190,
 		'width'       => 190,
@@ -12,32 +12,32 @@ if(! function_exists("theme_setup")){
 		add_theme_support('title-tag');
 		add_theme_support('post-thumbnails',array("post","page","movie"));
 	}
-	add_action('after_setup_theme','theme_setup');
+	add_action('after_setup_theme','WP_THEME_SquarePUNguin_theme_setup');
 }
 //места под выпадающие меню
-function theme_menus(){
+function WP_THEME_SquarePUNguin_theme_menus(){
 	$locations=array(
 		"Header" => __('Header Menue','Theme'),
 		"Footer" => __('Footer Menue',"Theme"),
 	);
 	register_nav_menus($locations);
 }
-add_action("init","theme_menus");
+add_action("init","WP_THEME_SquarePUNguin_theme_menus");
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
-function enqueue_comment_reply_script() {
+function WP_THEME_SquarePUNguin_enqueue_comment_reply_script() {
     if ( get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment_reply' );
     }
 }
-add_action( 'comment_form_before', 'enqueue_comment_reply_script' );
+add_action( 'comment_form_before', 'WP_THEME_SquarePUNguin_enqueue_comment_reply_script' );
 function theme_scripts() {
 	
 	wp_enqueue_script( "jquery",true );
 	wp_enqueue_style("maincss",get_stylesheet_uri(),array());
 	wp_enqueue_script("mainjs",get_template_directory_uri()."/scripts/main.js",array("jquery"),true);
 }
-function enqueue_comment_reply() {
+function WP_THEME_SquarePUNguin_enqueue_comment_reply() {
 		wp_enqueue_script('comment-reply');
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_comment_reply' );
+add_action( 'wp_enqueue_scripts', 'WP_THEME_SquarePUNguin_enqueue_comment_reply' );
 ?>
