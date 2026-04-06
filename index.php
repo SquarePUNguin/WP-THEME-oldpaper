690ce04d34b692801dffb9cba0425e97448e94ca
Dima <D_volk_2000@mail.ru>
2026-04-06 13:15:03 +0300

    another_round_of_fixes

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <html>
-	<!DOCTYPE html>
-	<head>
-		<?php wp_head(); ?>
-		<meta charset="UTF-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
-		</head>
-	<body>
-		<?php get_header();?>
-		<div class="main">
-			<?php the_content() ?>
-		</div>
-		<?php get_footer();?>
-		<?php wp_footer();?>
-	</body>
+<!DOCTYPE html>
+
+<head>
+  <?php wp_head(); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+  <?php wp_body_open(); ?>
+  <?php get_header(); ?>
+  <div class="main">
+    <?php the_content() ?>
+  </div>
+  <?php get_footer(); ?>
+  <?php wp_footer(); ?>
+</body>
+
 </html>
 <style>
-	.main{
-		--margin:1%;
-		margin-left:var(--margin);
-		margin-right:var(--margin);
-	}
+  .main {
+    --margin: 1%;
+    margin-left: var(--margin);
+    margin-right: var(--margin);
+  }
 </style>
